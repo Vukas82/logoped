c5a401272496a0adb9c8af6d9c9c9e434c37595c
Vukas82 <vukas82@gmail.com>
2019-11-06 15:40:22 +0100

    first

--- src/js/file1.js
+++ src/js/file1.js
@@ -220,3 +220,50 @@ dotsNav.addEventListener('click', e => {
 */
 
 // end of carousel
+
+
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function () {
+    stickyHeader()
+};
+
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function stickyHeader() {
+    // Get the header
+    var header = document.getElementById("header");
+    var slider = document.querySelector(".slider");
+
+    // Get the offset position of the navbar
+    var sticky = header.offsetTop + 500;
+
+    // console.log('scroll')
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+        slider.style.marginTop = '80px';
+    } else {
+        header.classList.remove("sticky");
+        slider.style.marginTop = '0px';
+
+    }
+}
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function stickyHeaderRefense() {
+    // Get the header
+    var header = document.getElementById("header");
+    var slider = document.querySelector(".viewport");
+
+    // Get the offset position of the navbar
+    var sticky = header.offsetTop + 330;
+
+    // console.log('scroll')
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+        slider.style.marginTop = '60px';
+    } else {
+        header.classList.remove("sticky");
+        slider.style.marginTop = '0px';
+
+    }
+}
